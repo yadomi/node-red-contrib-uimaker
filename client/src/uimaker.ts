1f9c6ad743665968c9a89b139c48a3d3242abde9
Felix Yadomi <felix.yadomi@gmail.com>
2020-03-14 16:07:46 +0100

    Codestyle + Cast event type to MessageEvent

--- client/src/uimaker.ts
+++ client/src/uimaker.ts
@@ -1,61 +1,57 @@
 export interface Node {
-  dispatch: (value: string | number | boolean) => Promise<Response>
-  subscribe: (listener: (value: string | number | boolean) => void) => void
+  dispatch: (value: string | number | boolean) => Promise<Response>;
+  subscribe: (listener: (value: string | number | boolean) => void) => void;
 }
 
 export const configure = (host: string) => (identifier: string) => {
-  const es = new EventSource(new URL('/uimaker-stream', host).href)
-  let listeners: Function[] = []
+  const es = new EventSource(new URL("/uimaker-stream", host).href);
+  let listeners: Function[] = [];
 
-  es.onerror = err => console.log({ err })
+  es.onerror = err => console.log({ err });
 
   const update = value => {
     listeners.forEach(listener => {
-      listener(value)
-    })
-  }
+      listener(value);
+    });
+  };
 
-  es.addEventListener('init', event => {
-    if (!(event instanceof MessageEvent)) return
-
-    const data = JSON.parse(event.data)
+  es.addEventListener("init", (event: MessageEvent) => {
+    const data = JSON.parse(event.data);
     if (data[identifier]) {
-      update(data[identifier])
+      update(data[identifier]);
     }
-  })
+  });
 
-  es.addEventListener('update', event => {
-    if (!(event instanceof MessageEvent)) return
-
-    const data = JSON.parse(event.data)
+  es.addEventListener("update", (event: MessageEvent) => {
+    const data = JSON.parse(event.data);
 
     if (data.nodeId === identifier) {
-      update(data.payload)
+      update(data.payload);
     }
-  })
+  });
 
   return {
-    dispatch (value) {
+    dispatch(value) {
       const message = {
         identifier: identifier,
         payload: value
-      }
+      };
 
-      return fetch(new URL('/uimaker-input', host).href, {
-        method: 'POST',
+      return fetch(new URL("/uimaker-input", host).href, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         },
         body: JSON.stringify(message)
-      })
+      });
     },
-    subscribe (listener) {
-      listeners.push(listener)
+    subscribe(listener) {
+      listeners.push(listener);
       const unsubscribe = () => {
-        listeners = listeners.filter(l => l !== listener)
-      }
+        listeners = listeners.filter(l => l !== listener);
+      };
 
-      return unsubscribe
+      return unsubscribe;
     }
-  }
-}
+  };
+};
